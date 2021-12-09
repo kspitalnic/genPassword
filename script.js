c433892258468f576e20c9f727a4fd44c99e638b
Kate Spitalnic <kespitalnic@gmail.com>
2021-12-09 17:07:43 -0500

    functioning password generator

--- script.js
+++ script.js
@@ -4,120 +4,123 @@
 //how to make code start over if something is invalid  
 //does it all stay within one function? (local/global?)
 
-var charAmountString = window.prompt ("How many characters would you like your password to have? (Please enter a numerical value that is greater than or equal to 8 and less than or equal to 128)."); 
-  console.log("charAmount input = " + charAmountString); 
-  var charAmount=parseInt(charAmountString);
-  console.log(charAmount);
-  console.log(charAmount>=8);
-  console.log(charAmount<=128);
+function generatePassword() {
 
-  if (charAmount>=8 && charAmount<=128) {
+  var charAmountString = window.prompt("How many characters would you like your password to have? (Please enter a numerical value that is greater than or equal to 8 and less than or equal to 128).");
+  console.log("charAmount input = " + charAmountString);
+  var charAmount = parseInt(charAmountString);
+  console.log(charAmount);
+  console.log(charAmount >= 8);
+  console.log(charAmount <= 128);
+
+  if (charAmount >= 8 && charAmount <= 128) {
     window.alert("Great! Your password will be " + charAmount + " characters long.");
-    console.log("Var  charAmount = " + charAmount)   }
+    console.log("Var  charAmount = " + charAmount)
+  }
   else {
     window.alert("Invalid input. To generate a password, please refresh the browser and try again with an acceptable number.");
   }
+  //return null
 
   var pwdCharCount = [];
   var charSet = [];
-  var pwdChars=[];
+  var pwdChars = [];
 
 
-window.alert("There are 4 character types: Uppercase, Lowercase, Numbers, and Special Characters. You must agree to include at least one character type in order to generate your password. You may agree to have all character types in your password (this is recommended for increased password protection). For each alert, press 'OK' if you would like the character type in your password. If not, press 'Cancel'.");
+  window.alert("There are 4 character types: Uppercase, Lowercase, Numbers, and Special Characters. You must agree to include at least one character type in order to generate your password. You may agree to have all character types in your password (this is recommended for increased password protection). For each alert, press 'OK' if you would like the character type in your password. If not, press 'Cancel'.");
 
-var upperChar=window.confirm ("Would you like to include uppercase letters in your password?");
+  var upperChar = window.confirm("Would you like to include uppercase letters in your password?");
   if (upperChar) {
     console.log("Include uppercase.");
   }
-  else{
+  else {
     console.log("Do not include uppercase.")
   }
 
-var lowerChar=window.confirm ("Would you like to include lowercase letters in your password?");
+  var lowerChar = window.confirm("Would you like to include lowercase letters in your password?");
   if (lowerChar) {
     console.log("Include lowercase.");
   }
-  else{
+  else {
     console.log("Do not include lowercase.")
   }
 
-var numChar=window.confirm ("Would you like to include numerical characters in your password?");
+  var numChar = window.confirm("Would you like to include numerical characters in your password?");
   if (numChar) {
     console.log("Include numbers.");
   }
-  else{
+  else {
     console.log("Do not include numbers.")
   }
 
-var specChar=window.confirm ("Would you like to include special characters in your password?");
+  var specChar = window.confirm("Would you like to include special characters in your password?");
   if (specChar) {
     console.log("Include special characters.");
   }
-  else{
+  else {
     console.log("Do not include special characters.")
   }
 
+  function determineChars() {
 
-function determineChars(){
-
-  //Uppercase Characters Array
+    //Uppercase Characters Array
     if (upperChar) {
-      upperSet= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-      pwdCharCount=pwdCharCount+1;
-    
-    var randomUpper = upperSet[Math.floor(Math.random()*upperSet.length)];
-    pwdChars.push(randomUpper);
+      upperSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+      pwdCharCount = pwdCharCount + 1;
+  
+      var randomUpper = upperSet[Math.floor(Math.random() * upperSet.length)];
+      pwdChars.push(randomUpper);
     }
     else {
-      upperSet=[];
-      pwdCharCount=pwdCharCount;
+      upperSet = [];
+      pwdCharCount = pwdCharCount;
     }
-
-
-    
-  //Lowercase Characters Array
-    if(lowerChar) {
-      lowerSet= 'abcdefghijklmnopqrstuvwxyz'.split('');
-      pwdCharCount=pwdCharCount+1;
-    var randomLower = lowerSet[Math.floor(Math.random()*lowerSet.length)];
-    pwdChars.push(randomLower);
+  
+  
+  
+    //Lowercase Characters Array
+    if (lowerChar) {
+      lowerSet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+      pwdCharCount = pwdCharCount + 1;
+      var randomLower = lowerSet[Math.floor(Math.random() * lowerSet.length)];
+      pwdChars.push(randomLower);
     }
-    else{
-      lowerSet=[];
-      pwdCharCount=pwdCharCount;
+    else {
+      lowerSet = [];
+      pwdCharCount = pwdCharCount;
     }
-
-
+  
+  
     //Numerical Charicters Array
     if (numChar) {
-      numSet='0123456789'.split('');
-      pwdCharCount=pwdCharCount+1;
-    
-    var randomNum = numSet[Math.floor(Math.random()*numSet.length)];
-    pwdChars.push(randomNum);
-
-    }
-    else{
-      numSet=[];
-      pwdCharCount=pwdCharCount;
-    }
-
-
-    //Special Characters Array 
-    if (specChar){
-      specSet=" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split('');
-      specSet.push('"');
-      pwdCharCount=pwdCharCount+1;
-    
-    var randomSpec = specSet[Math.floor(Math.random()*specSet.length)];
-    pwdChars.push(randomSpec);
+      numSet = '0123456789'.split('');
+      pwdCharCount = pwdCharCount + 1;
+  
+      var randomNum = numSet[Math.floor(Math.random() * numSet.length)];
+      pwdChars.push(randomNum);
+  
     }
     else {
-      specSet=[];
-      pwdCharCount=pwdCharCount;
+      numSet = [];
+      pwdCharCount = pwdCharCount;
     }
-    charSet=charSet.concat(upperSet, lowerSet, numSet, specSet)
-
+  
+  
+    //Special Characters Array 
+    if (specChar) {
+      specSet = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split('');
+      specSet.push('"');
+      pwdCharCount = pwdCharCount + 1;
+  
+      var randomSpec = specSet[Math.floor(Math.random() * specSet.length)];
+      pwdChars.push(randomSpec);
+    }
+    else {
+      specSet = [];
+      pwdCharCount = pwdCharCount;
+    }
+    charSet = charSet.concat(upperSet, lowerSet, numSet, specSet)
+  
     console.log(upperSet);
     console.log(lowerSet);
     console.log(numSet);
@@ -125,49 +128,41 @@ function determineChars(){
     console.log(pwdCharCount.length);
     console.log("current characters in array: " + pwdChars);
     console.log("charSet = " + charSet);
-
-    // let pwdChars = pwdChars.toString();
-    // console.log(pwdChars);
-
-    if (pwdCharCount.length>=1){
-      var charRemain = charAmount-pwdCharCount.length;
-      console.log("You need " + charRemain  + " more random characters before generating a password");
+  
+  
+    console.log(pwdChars);
+  
+    if (pwdCharCount.length >= 1) {
+      var charRemain = charAmount - pwdCharCount.length;
+      console.log("Will generate" + charRemain + " more random characters before generating a password");
     }
-    else {  
+    else {
       window.alert("You are required to accept at least one of the types of characters. A password will not generate. Please try again and confirm what types of characters you would like in your password.")
     }
+  
+  
+    console.log(charRemain);
+  
+    for (let i = 0; i < charRemain; i++) {
+      pwdChars.push(charSet[Math.floor(Math.random() * charSet.length)]);
+    }
+  
+    console.log(pwdChars);
+  
+    pwdChars = pwdChars.join("");
+  
+    console.log(pwdChars);
+  
+  
+    return pwdChars;
+  }
 
-
-console.log(charRemain);
-
-for (let i=0; i < charRemain-1; i++ ) {
-  pwdChars = pwdChars + charSet[Math.floor(Math.random()*charSet.length)]
+  var generatedPassword = determineChars();
+  return generatedPassword
 }
 
-console.log(pwdChars);
-
-}
-
-determineChars();
 
 
-// shuffle(pwdChars);
-
-// function shuffle() {
-//   while (pwdChars != 0) {
-//     pwd = Math.floor(Math.random() * pwdChars);
-//     pwdChars--;
-//   }
-// }
-
-
-
-//in this big set we will randomly select characters FOR the 
-//amount of characters needed to make the length of characters thety requested. 
-
-//once we have all the characters in an array. we will join them (to make them a string)
-//then we will randomize their order.
-//this will be the generated password.
 
 
 
@@ -182,8 +177,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+  
 
-// }
-
-// // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);}
+  // // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
